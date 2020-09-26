ef87e3d6b8eb6c3cb355526db0cf72dccd96aba6
Ivan M <ivmsystem@yandex.ru>
2020-09-26 15:50:49 +0300

    Init

--- src/pages/Profile/ProfilePage.js
+++ src/pages/Profile/ProfilePage.js
@@ -6,7 +6,7 @@ function ProfilePage() {
 
     return (
         <div>
-            <img src="https://pm1.narvii.com/6820/23b459f5e50f3ca2579a927b11064e0774356cfbv2_hq.jpg" alt="" />
+            <img src="https://pm1.narvii.com/6820/23b459f5e50f3ca2579a927b11064e0774356cfbv2_hq.jpg" alt="yes" />
         </div>
 )
 }
